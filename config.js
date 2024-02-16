5e8b49fdbd343e2dca04153c87143865a278e770
Sandesh7969 <155588072+Sandesh7969@users.noreply.github.com>
2024-02-16 19:58:21 +0545

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ module.exports = {
   Name: "Name Here!",
   State: "State Here!",
   Details: "Details Here!",
-  FirstButtonName: "Button1 Name",
-  FirstButtonUrl: "https://spicydevs.me/",
-  SecondButtonName: "Button2 Name",
-  SecondButtonUrl: "https://spicydevs.me/",
+  FirstButtonName: "Subscribe",
+  FirstButtonUrl: "https://m.youtube.com/@Supreme-guide",
+  SecondButtonName: "Join",
+  SecondButtonUrl: "https://discord.gg/GPzYFx7zfe",
   LargeImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453767412621362/c195de24559e92ba8f283a719d2e5137.png", // DISCORD CDN IMAGE ONLY
   LargeText: "Large Image Text", // hover text for large image
   SmallImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453635732451429/9b92f310a29c0dbcab36e825cb62593b.png", // DISCORD CDN IMAGE ONLY
