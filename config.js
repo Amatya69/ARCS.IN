3bb7046f40e171693e08f301030142d63d99a6de
Sandesh7969 <155588072+Sandesh7969@users.noreply.github.com>
2024-02-16 20:05:27 +0545

    Update config.js

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
   FirstButtonUrl: "https://m.youtube.com/@Supreme-guide",
   SecondButtonName: "Join",
   SecondButtonUrl: "https://discord.gg/GPzYFx7zfe",
-  LargeImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453767412621362/c195de24559e92ba8f283a719d2e5137.png", // DISCORD CDN IMAGE ONLY
+  LargeImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054958432325683/download_1.jpg?ex=65e1e3da&is=65cf6eda&hm=32a0ba9cd14e47ca93013c1584b3c61612ded64f7dedf5642776d58c50a5531b&", // DISCORD CDN IMAGE ONLY
   LargeText: "Large Image Text", // hover text for large image
-  SmallImage: "https://media.discordapp.net/attachments/1154789029963890802/1155453635732451429/9b92f310a29c0dbcab36e825cb62593b.png", // DISCORD CDN IMAGE ONLY
+  SmallImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054969429786625/906361.png?ex=65e1e3dd&is=65cf6edd&hm=3e44c8b3e0ef8eec693e4626497771d5ba8a96fe19c913a30d218813ae44947d&", // DISCORD CDN IMAGE ONLY
   SmallText: "Small Image Text", // hover text for small image
 };
 
