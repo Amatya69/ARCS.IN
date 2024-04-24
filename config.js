e6fe7e3fe984cb2d4e1803fdc46ca6422b6b9edb
AmatyaSir <167998184+AmatyaSir@users.noreply.github.com>
2024-04-24 21:13:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,15 +9,15 @@ module.exports = {
   showTime: false, // Toggle to show or hide time in status (true/false)
   token: "" || process.env.TOKEN,
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
-  Name: "Moderation",
-  State: "Bot Setups",
-  Details: "Discord Moderation",
-  FirstButtonName: "Subscribe",
-  FirstButtonUrl: "https://m.youtube.com/@Supreme-guide",
-  SecondButtonName: "Join",
-  SecondButtonUrl: "https://discord.gg/GPzYFx7zfe",
+  Name: "AstraVerse Services",
+  State: "Team Builds",
+  Details: "Team AstraVerse",
+  FirstButtonName: "Subscribe Us",
+  FirstButtonUrl: "https://youtube.com/@AstraVerseOfficial?si=cJc__EXtcrhasFpE",
+  SecondButtonName: "Follow Us",
+  SecondButtonUrl: "https://www.instagram.com/astraverse.in?igsh=MWgwMW96bTczYXVnZw==",
   LargeImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054958432325683/download_1.jpg?ex=65e1e3da&is=65cf6eda&hm=32a0ba9cd14e47ca93013c1584b3c61612ded64f7dedf5642776d58c50a5531b&", // DISCORD CDN IMAGE ONLY
-  LargeText: "Fun cmnds", // hover text for large image
+  LargeText: "FaithAtLoyalty", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054969429786625/906361.png?ex=65e1e3dd&is=65cf6edd&hm=3e44c8b3e0ef8eec693e4626497771d5ba8a96fe19c913a30d218813ae44947d&", // DISCORD CDN IMAGE ONLY
   SmallText: "Small Image Text", // hover text for small image
 };
