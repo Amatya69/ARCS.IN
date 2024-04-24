dcba696a3470cc4569013c896ceb01e383650115
AmatyaSir <167998184+AmatyaSir@users.noreply.github.com>
2024-04-24 21:17:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
   FirstButtonUrl: "https://youtube.com/@AstraVerseOfficial?si=cJc__EXtcrhasFpE",
   SecondButtonName: "Follow Us",
   SecondButtonUrl: "https://www.instagram.com/astraverse.in?igsh=MWgwMW96bTczYXVnZw==",
-  LargeImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054958432325683/download_1.jpg?ex=65e1e3da&is=65cf6eda&hm=32a0ba9cd14e47ca93013c1584b3c61612ded64f7dedf5642776d58c50a5531b&", // DISCORD CDN IMAGE ONLY
+  LargeImage: "https://cdn.discordapp.com/attachments/1226947050265120818/1227571654364893215/1706435399247.jpg?ex=662a35b1&is=6628e431&hm=ba48308a6d9924d2c6bb7b39bd207ec833f6904bb677cddeceeb2c90f6ba5738&", // DISCORD CDN IMAGE ONLY
   LargeText: "FaithAtLoyalty", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054969429786625/906361.png?ex=65e1e3dd&is=65cf6edd&hm=3e44c8b3e0ef8eec693e4626497771d5ba8a96fe19c913a30d218813ae44947d&", // DISCORD CDN IMAGE ONLY
   SmallText: "Small Image Text", // hover text for small image
