7257dce707f71487ad49134da0cb488fa23d0c68
Amatya69 <155410394+Amatya69@users.noreply.github.com>
2024-06-08 12:58:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,15 +9,15 @@ module.exports = {
   showTime: false, // Toggle to show or hide time in status (true/false)
   token: "" || process.env.TOKEN,
   timeZone: "Asia/Kolkata", //Your Timezone, eg Asia/Kolkata
-  Name: "AstraVerse Services",
-  State: "Team Builds",
-  Details: "Team AstraVerse",
+  Name: "Arceus eSprots",
+  State: "ARCS-E",
+  Details: "Team Arceus",
   FirstButtonName: "Subscribe Us",
-  FirstButtonUrl: "https://youtube.com/@AstraVerseOfficial?si=cJc__EXtcrhasFpE",
+  FirstButtonUrl: "https://youtube.com/@gameworri?si=MSZvsQmOLUbGGOH5",
   SecondButtonName: "Follow Us",
   SecondButtonUrl: "https://www.instagram.com/astraverse.in?igsh=MWgwMW96bTczYXVnZw==",
   LargeImage: "https://cdn.discordapp.com/attachments/1226947050265120818/1227571654364893215/1706435399247.jpg?ex=662a35b1&is=6628e431&hm=ba48308a6d9924d2c6bb7b39bd207ec833f6904bb677cddeceeb2c90f6ba5738&", // DISCORD CDN IMAGE ONLY
-  LargeText: "FaithAtLoyalty", // hover text for large image
+  LargeText: "LetGameAwake!", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/attachments/1197875910184022016/1208054969429786625/906361.png?ex=65e1e3dd&is=65cf6edd&hm=3e44c8b3e0ef8eec693e4626497771d5ba8a96fe19c913a30d218813ae44947d&", // DISCORD CDN IMAGE ONLY
   SmallText: "Small Image Text", // hover text for small image
 };
